77c697d8ab2a1f9732b0a3bd20b03286548a8100
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-04-14 21:13:17 -0300

    initial commit

---
+++ src/main/java/br/com/victorgadelha/primeiroprojetospringboot/PrimeiraController.java
@@ -0,0 +1,10 @@
+package br.com.victorgadelha.primeiroprojetospringboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/primeiraController")
+public class PrimeiraController {
+
+}
---
+++ src/test/java/br/com/victorgadelha/primeiroprojetospringboot/PrimeiroProjetoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victorgadelha.primeiroprojetospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
