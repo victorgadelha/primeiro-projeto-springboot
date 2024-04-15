8380f8e81b102def2379d45715b11659d5fd7b26
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-04-14 21:48:23 -0300

    feat: Criando record para usuário

--- src/main/java/br/com/victorgadelha/controller/PrimeiraController.java
+++ src/main/java/br/com/victorgadelha/controller/PrimeiraController.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +30,11 @@ public class PrimeiraController {
     }
 
     @PostMapping("/metodoComBodyParams")
-    public String metodoComBodyParams() {
-        return "metodoComBodyParams";
+    public String metodoComBodyParams(@RequestBody Usuario usuario) {
+        return "metodoComBodyParams: " + usuario.username();
+    }
+
+    record Usuario(String username) {
+
     }
 }
