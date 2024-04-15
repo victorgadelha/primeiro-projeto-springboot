1827e2489183af7326e35b54966428ec22f57910
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-04-14 21:18:20 -0300

    feat: Criando primeira rota com springboot

--- src/main/java/br/com/victorgadelha/primeiroprojetospringboot/PrimeiraController.java
+++ src/main/java/br/com/victorgadelha/primeiroprojetospringboot/PrimeiraController.java
@@ -1,5 +1,6 @@
 package br.com.victorgadelha.primeiroprojetospringboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/primeiraController")
 public class PrimeiraController {
 
+    @GetMapping("primeiroMetodo")
+    public String primeiroMetodo() {
+        return "Parabéns, você criou a sua primeira rota!";
+    }
 }
