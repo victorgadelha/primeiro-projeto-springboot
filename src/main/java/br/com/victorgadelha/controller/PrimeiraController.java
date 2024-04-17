24d81471675f6a9a8e966d961693e421d0be5eba
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-04-16 21:35:38 -0300

    feat: Criando método de ResponseEntity

--- src/main/java/br/com/victorgadelha/controller/PrimeiraController.java
+++ src/main/java/br/com/victorgadelha/controller/PrimeiraController.java
@@ -2,10 +2,13 @@ package br.com.victorgadelha.controller;
 
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +38,28 @@ public class PrimeiraController {
     }
 
     record Usuario(String username) {
+
+    }
+
+    @PostMapping("/metodoComBodyHeaders")
+    public String metodoComBodyParams(@RequestHeader("name") String name) {
+        return "metodoComHeaders: " + name;
+    }
+
+    @PostMapping("/metodoComListHeaders")
+    public String metodoComBodyParams(@RequestHeader Map<String, String> headers) {
+        return "metodoComHeaders: " + headers.entrySet();
+    }
+
+    @GetMapping("/metodoResponseEntity/{id}")
+    public ResponseEntity<Object> metodoResponseEntity(@PathVariable Long id) {
+        var usuario = new Usuario("João Victor");
+
+        if (id > 5) {
+            return ResponseEntity.status(HttpStatus.OK).body(usuario);
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Número menor que 5");
 
     }
 }
